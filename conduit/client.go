5199210123c8b9d02218243976754152c1b2ecb1
Allen Plummer <ahplummer@gmail.com>
2021-03-20 08:12:00 -0500

    Fixed bearer token header

--- conduit/client.go
+++ conduit/client.go
@@ -2,6 +2,7 @@ package conduit
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,6 +15,20 @@ func (s DatabasesStruct) Print() {
 		fmt.Printf("Database: %v\n", v)
 	}
 }
+//type TableStruct struct {
+//	Table string
+//	Database string
+//	Schema string
+//	TableType string
+//}
+//type TablesStruct struct {
+//	Tables
+//}
+//func (s TableStruct) Print() {
+//	fmt.Printf("Table: %v, from db: %v, is a part of schema: %v, and is of type: %v",
+//		s.Table, s.Database, s.Schema, s.TableType)
+//}
+
 type ConduitClient struct {
 	ConduitServer string
 	ConduitToken string
@@ -37,19 +52,24 @@ func (c *ConduitClient) GetOnTheWire(endpoint string, target interface{}) (err e
 		return err
 	}
 	req.Header.Set("accept", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf("Token %s", c.ConduitToken))
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.ConduitToken))
 	resp, err := httpClient.Do(req)
-
 	if err != nil {
 		log.Printf("Error doing request: %s", err.Error())
 		return err
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		errstring := fmt.Sprintf("Status Code %v returned", resp.StatusCode)
+		log.Printf(errstring)
+		return errors.New(errstring)
+	}
 	json.NewDecoder(resp.Body).Decode(target)
 	return nil
 }
 func (c ConduitClient) GetDatabases() *DatabasesStruct {
-	curlstring := "curl -X GET 'https://$CONDUIT_SERVER/query/metadata/databases' -H  'accept: application/json' -H 'Authorization: Token $CONDUIT_TOKEN'"
+	curlstring := "curl -X GET \"https://$CONDUIT_SERVER/query/metadata/databases\" -H  \"accept: application/json\" -H \"Authorization: Bearer $CONDUIT_TOKEN\""
 	databases := new(DatabasesStruct)
 	err := c.GetOnTheWire("/metadata/databases", databases)
 	if err != nil {
@@ -57,3 +77,8 @@ func (c ConduitClient) GetDatabases() *DatabasesStruct {
 	}
 	return databases
 }
+//func (c ConduitClient) GetTables(database string) {
+//	curlstring := "curl -X GET \"https://$CONDUIT_SERVER/query/metadata/databases/{database}/tables\" -H  \"accept: application/json\" -H \"Authorization: Token $CONDUIT_TOKEN\""
+//	tables := new(TablesStruct)
+//	err := c.GetOnTheWire(fmt.Sprintf("/metadata/databases/%s/tables",database), tables)
+//}
